100defd67f43629fb5faaf803acfc603be97f0b9
bentouyaP <bentouyachannel@gmail.com>
2019-02-05 01:13:37 +0900

    atreus62 keymap add macros.

--- keyboards/atreus62/keymaps/bentouya_p/keymap.c
+++ keyboards/atreus62/keymaps/bentouya_p/keymap.c
@@ -32,7 +32,7 @@ enum atreus62_keycodes {
 
 // Custom
 #define SFT_ENT SFT_T(KC_ENT)  // Tap for Enter, hold for Shift
-#define FNC MO(_FUNCTION)  // FUNCTION layer
+#define FNC MO(_FUNCTION)  // Momentarily turn on FUNCTION layer
 #define KANA KC_INT2  // JIS Katakana/Hiragana
 #define E_J KC_LANG5  // JIS Zenkaku/Hankaku
 //UnUse
@@ -52,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |XXXXXX|XXXXXX|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |XXXXXX|XXXXXX|   N  |   M  |   ,  |   .  |   /  | Shift|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |XXXXXX|XXXXXX|   N  |   M  |   ,  |   .  |   /  |Sf/Ent|
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * | Func | Left |Right | Alt  |Lower | Space| Del  | Enter| Bksp |Raise | GUI  | Down |  Up  | Ctrl |
+ * | Func | Alt  | Left |Right |Lower | Space| Del  | Enter| Bksp |Raise | Down |  Up  | GUI  | Ctrl |
  * `-------------------------------------------------------------------------------------------------'
  */
   [_QWERTY] = LAYOUT ( /* qwerty */
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT,
-    FNC,     KC_LEFT, KC_RGHT, KC_LALT, LOWER,   KC_SPC,  KC_DEL,  KC_ENT,  KC_BSPC, RAISE,   KC_RGUI, KC_DOWN, KC_UP,   KC_RCTL
+    FNC,     KC_LALT, KC_LEFT, KC_RGHT, LOWER,   KC_SPC,  KC_DEL,  KC_ENT,  KC_BSPC, RAISE,   KC_DOWN, KC_UP,   KC_RGUI, KC_RCTL
   ),
 /* Dvorak
  * ,-------------------------------------------------------------------------------------------------.
@@ -72,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl |   A  |   O  |   E  |   U  |   I  |XXXXXX|XXXXXX|   D  |   H  |   T  |   N  |   S  |   -  |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   ;  |   Q  |   J  |   K  |   X  |XXXXXX|XXXXXX|   B  |   M  |   W  |   V  |   Z  | Shift|
+ * | Shift|   ;  |   Q  |   J  |   K  |   X  |XXXXXX|XXXXXX|   B  |   M  |   W  |   V  |   Z  |Sf/Ent|
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * | Func | Left |Right | Alt  |Lower | Space| Del  | Enter| Bksp |Raise | GUI  | Down |  Up  | Ctrl |
+ * | Func | Alt  | Left |Right |Lower | Space| Del  | Enter| Bksp |Raise | Down |  Up  | GUI  | Ctrl |
  * `-------------------------------------------------------------------------------------------------'
  */
   [_DVORAK] = LAYOUT ( /* dvorak */
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
     KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
     KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                      KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    SFT_ENT,
-    FNC,     KC_LEFT, KC_RGHT, KC_LALT, LOWER,   KC_SPC,  KC_DEL,  KC_ENT,  KC_BSPC, RAISE,   KC_RGUI, KC_DOWN, KC_UP,   KC_RCTL
+    FNC,     KC_LALT, KC_LEFT, KC_RGHT, LOWER,   KC_SPC,  KC_DEL,  KC_ENT,  KC_BSPC, RAISE,   KC_DOWN, KC_UP,   KC_RGUI, KC_RCTL
   ),
 /* Colemak
  * ,-------------------------------------------------------------------------------------------------.
@@ -92,9 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl |   A  |   R  |   S  |   T  |   D  |XXXXXX|XXXXXX|   H  |   N  |   E  |   I  |   O  |  '   |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |XXXXXX|XXXXXX|   K  |   M  |   ,  |   .  |   /  | Shift|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |XXXXXX|XXXXXX|   K  |   M  |   ,  |   .  |   /  |Sf/Ent|
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * | Func | Left |Right | Alt  |Lower | Space| Del  | Enter| Bksp |Raise | GUI  | Down |  Up  | Ctrl |
+ * | Func | Alt  | Left |Right |Lower | Space| Del  | Enter| Bksp |Raise | Down |  Up  | GUI  | Ctrl |
  * `-------------------------------------------------------------------------------------------------'
  */
   [_COLEMAK] = LAYOUT ( /* colemak */
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
     KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT,
-    FNC,     KC_LEFT, KC_RGHT, KC_LALT, LOWER,   KC_SPC,  KC_DEL,  KC_ENT,  KC_BSPC, RAISE,   KC_RGUI, KC_DOWN, KC_UP,   KC_RCTL
+    FNC,     KC_LALT, KC_LEFT, KC_RGHT, LOWER,   KC_SPC,  KC_DEL,  KC_ENT,  KC_BSPC, RAISE,   KC_DOWN, KC_UP,   KC_RGUI, KC_RCTL
   ),
 
 /* Lower
@@ -115,15 +115,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |XXXXXX|XXXXXX|  F12 |ISO ~ |ISO | |JIS _ |JIS | |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Home | End  |      |      |Muhen | E/J  | KANA |Henkan|      | MENU |Pg-dwn| Pg-up|      |
+ * |      |      | Home | End  |      |Muhen | E/J  | KANA |Henkan|      |Pg-dwn| Pg-up|      |      |
  * `-------------------------------------------------------------------------------------------------'
  */
-  [_LOWER] = LAYOUT (
+  [_LOWER] = LAYOUT ( /* lower */
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
     KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,                    KC_F12,  S(KC_NUHS),S(KC_NUBS), S(KC_RO), S(KC_JYEN), _______,
-    _______, KC_HOME, KC_END,  _______, _______, KC_MHEN, E_J,    KANA,     KC_HENK, _______, KC_APP,  KC_PGDN, KC_PGUP, _______
+    _______, _______, KC_HOME, KC_END,  _______, KC_MHEN, E_J,    KANA,     KC_HENK, _______, KC_PGDN, KC_PGUP, _______, _______
   ),
 
 /* Raise
@@ -134,49 +134,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |XXXXXX|XXXXXX|  F6  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |XXXXXX|XXXXXX|  F12 |ISO # |ISO / |JIS \ |JIS \ |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |XXXXXX|XXXXXX|  F12 |ISO # |ISO / |JIS \ |JIS Y |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Home | End  |      |      |Muhen | E/J  | KANA |Henkan|      | MENU |Pg-dwn| Pg-up|      |
+ * |      |      | Home | End  |      |Muhen | E/J  | KANA |Henkan|      |Pg-dwn| Pg-up|      |      |
  * `-------------------------------------------------------------------------------------------------'
  */
-  [_RAISE] = LAYOUT (
+  [_RAISE] = LAYOUT ( /* raise */
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,                    KC_F12,  KC_NUHS, KC_NUBS, KC_RO,   KC_JYEN, _______,
-    _______, KC_HOME, KC_END,  _______, _______, KC_MHEN, E_J,    KANA,     KC_HENK, _______, KC_APP,  KC_PGDN, KC_PGUP, _______
+    _______, _______, KC_HOME, KC_END,  _______, KC_MHEN, E_J,    KANA,     KC_HENK, _______, KC_PGDN, KC_PGUP, _______, _______
   ),
 
 /* Function
  * ,-------------------------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |XXXXXX|XXXXXX| Caps | SCLK | PAUSE| INS  |      |      |
+ * |      |      |      |      |      |      |XXXXXX|XXXXXX| Caps | SCLK | PAUSE| INS  |      | Mute |
  * |-------------------------------------------------------------------------------------------------|
- * |      | M0   | M1   | M2   |      | M5   |XXXXXX|XXXXXX|      | Pg-up|  Up  |Pg-dwn| PSCR |      |
+ * |      | M0   | M1   | M2   | M3   | M4   |XXXXXX|XXXXXX| MENU | Pg-up|  Up  |Pg-dwn| PSCR |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | M4   | M3   | M6   |      |      |XXXXXX|XXXXXX| Home | Left | Down |Right | End  | Enter|
+ * |      | M5   | M6   | M7   | M8   | M9   |XXXXXX|XXXXXX| Home | Left | Down |Right | End  | Enter|
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      | M9   | M7   | M8   |XXXXXX|XXXXXX|      | Stop | Prev | Next | Play |      |
+ * |      | M10  | M11  | M12  | M13  | M14  |XXXXXX|XXXXXX|      | Stop | Prev | Next | Play |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      | Mute | Vol- | Vol+ |      |
+ * |      |      | M15  | M16  |      |      | M17  |      |      |      | Vol- | Vol+ |      |      |
  * `-------------------------------------------------------------------------------------------------'
- */
 //Macros:
-//M(0) //Ctrl + K, Ctrl + D
-//M(1) //Ctrl + K, Ctrl + C 
-//M(2) //Ctrl + K, Ctrl + U
-//M(3) //F2
-//M(4) //Shft + F2
-//M(5) //Ctrl + F4
-//M(6) //F12
-//M(7) //Ctrl+Alt+PageDown
-//M(8) //Ctrl+Alt+PageUp
-//M(9) //Ctrl + M, Ctrl + M
-  [_FUNCTION] = LAYOUT (
-    _______, _______, _______, _______, _______, _______,                   KC_CAPS, KC_SLCK, KC_PAUS, KC_INS, _______, _______,
-    _______, M(0),    M(1),    M(2),    _______, M(5),                      _______, KC_PGUP, KC_UP,   KC_PGDN, KC_PSCR, _______,
-    _______, M(4),    M(3),    M(6),    _______, _______,                   KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_ENT,
-    _______, _______, _______, M(9),    M(7),    M(8),                      _______, KC_MSTP, KC_MPRV, KC_MNXT, KC_MPLY, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______
+//M(0)   F2				//M(1)   Shft + F2
+//M(2)   Shft + F2			//M(3)   Ctrl + K, Ctrl + C
+//M(4)   Ctrl + K, Ctrl + U		//M(5)   F12
+//M(6)   Ctrl + Shft + S		//M(7)   Ctrl + M, Ctrl + M
+//M(8)   Ctrl + Alt + PageDown		//M(9)   Ctrl + Alt + PageUp
+//M(10)  Ctrl + Z			//M(11)  Ctrl + X
+//M(12)  Ctrl + C			//M(13)  Ctrl + V
+//M(14)  Ctrl + Shft + B		//M(15)  Ctrl + "-"
+//M(16)  Ctrl + Shft + "-"		//M(17)  Ctrl + Alt + Delete
+*/
+  [_FUNCTION] = LAYOUT ( /* function */
+    _______, _______, _______, _______, _______, _______,                   KC_CAPS, KC_SLCK, KC_PAUS, KC_INS, _______, KC_MUTE,
+    _______, M(0),    M(1),    M(2),    M(3),    M(4),                      KC_APP,  KC_PGUP, KC_UP,   KC_PGDN, KC_PSCR, _______,
+    _______, M(5),    M(6),    M(7),    M(8),    M(9),                      KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_ENT,
+    _______, M(10),   M(11),   M(12),   M(13),   M(14),                     _______, KC_MSTP, KC_MPRV, KC_MNXT, KC_MPLY, _______,
+    _______, _______, M(15),   M(16),   _______, _______,  M(17),  _______, _______, _______, KC_VOLD, KC_VOLU, _______, _______
   ),
 
 /* Adjust (Lower + Raise)
@@ -192,7 +191,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-------------------------------------------------------------------------------------------------'
  */
-  [_ADJUST] = LAYOUT (
+  [_ADJUST] = LAYOUT ( /* adjust */
     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
     _______, RESET,   _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                   _______, QWERTY,  COLEMAK, DVORAK,  _______, _______,
@@ -209,54 +208,94 @@ const uint16_t PROGMEM fn_actions[] = {
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
     switch(id) {
-    case 0: //Ctrl + K, Ctrl + D
-        if (record->event.pressed) {// on keydown
-            return MACRO(D(LCTL), T(K), U(LCTL), D(LCTL), T(D), U(LCTL), END); 
-        }
-        break;
-    case 1: //Ctrl + K, Ctrl + C 
-        if (record->event.pressed) {// on keydown
-            return MACRO(D(LCTL), T(K), U(LCTL), D(LCTL), T(C), U(LCTL), END);
-        }
-        break;
-    case 2: //Ctrl + K, Ctrl + U
-        if (record->event.pressed) {// on keydown
-            return MACRO(D(LCTL), T(K), U(LCTL), D(LCTL), T(U), U(LCTL), END);
-        }
-        break;
-    case 3: //F2
+    case 0: //M(0) //F2
         if (record->event.pressed) {// on keydown
             return MACRO(T(F2), END);
         }
         break;
-    case 4: //Shft + F2
-        if (record->event.pressed) {// on keydown
-            return MACRO(D(LSFT), T(F2), U(LSFT), END);
-        }
-        break;
-    case 5: //Ctrl + F4
+    case 1: //M(1) //Ctrl + F4
         if (record->event.pressed) {// on keydown
             return MACRO(D(LCTL), T(F4), U(LCTL), END);
         }
         break;
-    case 6: //F12
+    case 2: //M(2) //Shft + F2
+        if (record->event.pressed) {// on keydown
+            return MACRO(D(LSFT), T(F2), U(LSFT), END);
+        }
+        break;
+    case 3: //M(3) //Ctrl + K, Ctrl + C
+        if (record->event.pressed) {// on keydown
+            return MACRO(D(LCTL), T(K), U(LCTL), D(LCTL), T(C), U(LCTL), END);
+        }
+        break;
+    case 4: //Ctrl + K, Ctrl + U
+        if (record->event.pressed) {// on keydown
+            return MACRO(D(LCTL), T(K), U(LCTL), D(LCTL), T(U), U(LCTL), END);
+        }
+        break;
+    case 5: //M(5) //F12
         if (record->event.pressed) {// on keydown
             return MACRO(T(F12), END);
         }
         break;
-    case 7: //Ctrl+Alt+PageDown
+    case 6: //M(6) //Ctrl + Shft + S
+        if (record->event.pressed) {// on keydown
+            return MACRO(D(LCTL), D(LSFT), T(S), U(LSFT), U(LCTL), END);
+        }
+        break;
+    case 7: //M(7) //Ctrl + M, Ctrl + M
+        if (record->event.pressed) {// on keydown
+            return MACRO(D(LCTL), T(M), U(LCTL), D(LCTL), T(M), U(LCTL), END);
+        }
+        break;
+    case 8: //M(8) //Ctrl + Alt + PageDown
         if (record->event.pressed) {// on keydown
             return MACRO(D(LCTL), D(LALT), T(PGDN), U(LALT), U(LCTL), END);
         }
         break;
-    case 8: //Ctrl+Alt+PageUp
+    case 9: //M(9) //Ctrl + Alt + PageUp
         if (record->event.pressed) {// on keydown
             return MACRO(D(LCTL), D(LALT), T(PGUP), U(LALT), U(LCTL), END);
         }
         break;
-    case 9: //Ctrl + M, Ctrl + M
+    case 10: //M(10)//Ctrl + Z
         if (record->event.pressed) {// on keydown
-            return MACRO(D(LCTL), T(M), U(LCTL), D(LCTL), T(M), U(LCTL), END);
+            return MACRO(D(LCTL), T(Z), U(LCTL), END);
+        }
+        break;
+    case 11: //M(11)//Ctrl + X
+        if (record->event.pressed) {// on keydown
+            return MACRO(D(LCTL), T(X), U(LCTL), END);
+        }
+        break;
+    case 12: //M(12)//Ctrl + C
+        if (record->event.pressed) {// on keydown
+            return MACRO(D(LCTL), T(C), U(LCTL), END);
+        }
+        break;
+    case 13: //M(13)//Ctrl + V
+        if (record->event.pressed) {// on keydown
+            return MACRO(D(LCTL), T(V), U(LCTL), END);
+        }
+        break;
+    case 14: //M(14)//Ctrl + Shft + B
+        if (record->event.pressed) {// on keydown
+            return MACRO(D(LCTL), D(LSFT), T(B), U(LSFT), U(LCTL), END);
+        }
+        break;
+    case 15: //M(15)//Ctrl + "-"
+        if (record->event.pressed) {// on keydown
+            return MACRO(D(LCTL), T(MINS), U(LCTL), END);
+        }
+        break;
+    case 16: //M(16)//Ctrl + Shft + "-"
+        if (record->event.pressed) {// on keydown
+            return MACRO(D(LCTL), D(LSFT), T(MINS), U(LSFT), U(LCTL), END);
+        }
+        break;
+    case 17: //M(17)//Ctrl + Alt + Delete
+        if (record->event.pressed) {// on keydown
+            return MACRO(D(LCTL), D(LALT), T(DEL), U(LALT), U(LCTL), END);
         }
         break;
     }
@@ -271,38 +310,38 @@ void persistent_default_layer_set(uint16_t default_layer) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
         case QWERTY:
-          if (record->event.pressed) {
+          if (record->event.pressed) {// on keydown
             persistent_default_layer_set(1UL<<_QWERTY);
           }
           return false;
           break;
         case COLEMAK:
-          if (record->event.pressed) {
+          if (record->event.pressed) {// on keydown
             persistent_default_layer_set(1UL<<_COLEMAK);
           }
           return false;
           break;
         case DVORAK:
-          if (record->event.pressed) {
+          if (record->event.pressed) {// on keydown
             persistent_default_layer_set(1UL<<_DVORAK);
           }
           return false;
           break;
         case LOWER:
-          if (record->event.pressed) {
+          if (record->event.pressed) {// on keydown
             layer_on(_LOWER);
             update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
+          } else {// on keyup
             layer_off(_LOWER);
             update_tri_layer(_LOWER, _RAISE, _ADJUST);
           }
           return false;
           break;
         case RAISE:
-          if (record->event.pressed) {
+          if (record->event.pressed) {// on keydown
             layer_on(_RAISE);
             update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
+          } else {// on keyup
             layer_off(_RAISE);
             update_tri_layer(_LOWER, _RAISE, _ADJUST);
           }
